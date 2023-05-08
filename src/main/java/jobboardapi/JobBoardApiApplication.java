b4344030e7a7d2ea3ea2f6da3d824339a817893c
Kevin Barrios <kevinbarrios@ymail.com>
2023-05-08 16:32:25 -0500

    Pushing initial code

---
+++ src/main/java/jobboardapi/JobBoardApiApplication.java
@@ -0,0 +1,13 @@
+package jobboardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBoardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobBoardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jobboardapi/JobBoardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jobboardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBoardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
