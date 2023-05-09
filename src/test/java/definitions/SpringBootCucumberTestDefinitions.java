4b0fbe050aee8435755294eccf1aef802509598d
dayjyun <kbarriosdev@gmail.com>
2023-05-08 19:31:42 -0500

    Created definitions folder and created SpringBootCucumberTestDefinitions package inside definitions folder

---
+++ src/test/java/definitions/SpringBootCucumberTestDefinitions.java
@@ -0,0 +1,9 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+
+public class SpringBootCucumberTestDefinitions {
+   @Given("A list of books are available")
+   public void aListOfBooksAreAvailable() {
+   }
+}
--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -2,7 +2,7 @@ package runners;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.runners.RunWith;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
