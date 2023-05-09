ed8f055a305026a9e645e2534ca5fa2cdd057d6a
dayjyun <kbarriosdev@gmail.com>
2023-05-08 19:23:48 -0500

    Added boilerplate code for TestRunner class in runners package

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,4 +1,14 @@
 package runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runners.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        plugin = {"pretty"}, dryRun = true,
+        glue = "definitions"
+)
 public class TestRunner {
 }
