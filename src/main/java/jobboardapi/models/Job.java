13fca9e93274a4d27432543df2c43da26bf15f60
dayjyun <kbarriosdev@gmail.com>
2023-05-08 19:59:17 -0500

    Created fields for Job class in models package

--- src/main/java/jobboardapi/models/Job.java
+++ src/main/java/jobboardapi/models/Job.java
@@ -1,4 +1,21 @@
 package jobboardapi.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Column;
+import java.sql.Timestamp;
+
 public class Job {
+   private Long id;
+   private String title;
+   private String description;
+   private String location;
+   private double salary;
+   private boolean applied;
+
+   @Column
+   @CreationTimestamp
+   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy HH-mm-ss")
+   private Timestamp createdAt;
 }
