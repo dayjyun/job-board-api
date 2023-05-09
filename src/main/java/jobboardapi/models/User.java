ffbe0d386b0470c92f579068ac638ce86b1a2c53
dayjyun <kbarriosdev@gmail.com>
2023-05-08 20:03:53 -0500

    Generated constructors in User class

--- src/main/java/jobboardapi/models/User.java
+++ src/main/java/jobboardapi/models/User.java
@@ -3,11 +3,21 @@ package jobboardapi.models;
 public class User {
    private Long id;
 
-   
+
    private String name;
 
 
    private String email;
    private String password;
    private String resume;
+
+   public User() {}
+
+   public User(Long id, String name, String email, String password, String resume) {
+      this.id = id;
+      this.name = name;
+      this.email = email;
+      this.password = password;
+      this.resume = resume;
+   }
 }
