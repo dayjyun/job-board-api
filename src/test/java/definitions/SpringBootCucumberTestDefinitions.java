9005b54c4c64d4a7cc16c5d8cf648e1cf1b7fffc
dayjyun <kbarriosdev@gmail.com>
2023-05-08 19:48:47 -0500

    Set up boilerplate code in SpringBootCucumberTestDefinitions file in definitions package

--- src/test/java/definitions/SpringBootCucumberTestDefinitions.java
+++ src/test/java/definitions/SpringBootCucumberTestDefinitions.java
@@ -1,8 +1,21 @@
 package definitions;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.spring.CucumberContextConfiguration;
+import io.restassured.response.Response;
+import jobboardapi.JobBoardApiApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = JobBoardApiApplication.class)
 public class SpringBootCucumberTestDefinitions {
+   private static final String BASE_URL = "http://localhost:";
+   private static Response response;
+
+   @LocalServerPort
+   String port;
+
    @Given("A list of books are available")
    public void aListOfBooksAreAvailable() {
    }
