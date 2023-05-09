3b0a22de2c9dc25d689b61ea3f5355012d98d545
dayjyun <kbarriosdev@gmail.com>
2023-05-08 20:04:19 -0500

    Generated getters and setters in User class in models package

--- src/main/java/jobboardapi/models/User.java
+++ src/main/java/jobboardapi/models/User.java
@@ -20,4 +20,44 @@ public class User {
       this.password = password;
       this.resume = resume;
    }
+
+   public Long getId() {
+      return id;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   public String getPassword() {
+      return password;
+   }
+
+   public void setPassword(String password) {
+      this.password = password;
+   }
+
+   public String getResume() {
+      return resume;
+   }
+
+   public void setResume(String resume) {
+      this.resume = resume;
+   }
 }
