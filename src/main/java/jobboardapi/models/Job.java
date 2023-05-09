b3b960b34c1f460a89616db3309e3fd0883841dc
dayjyun <kbarriosdev@gmail.com>
2023-05-08 19:59:58 -0500

    Generated constructors for Job class in models package

--- src/main/java/jobboardapi/models/Job.java
+++ src/main/java/jobboardapi/models/Job.java
@@ -18,4 +18,16 @@ public class Job {
    @CreationTimestamp
    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy HH-mm-ss")
    private Timestamp createdAt;
+
+   public Job() {}
+
+   public Job(Long id, String title, String description, String location, double salary, boolean applied, Timestamp createdAt) {
+      this.id = id;
+      this.title = title;
+      this.description = description;
+      this.location = location;
+      this.salary = salary;
+      this.applied = applied;
+      this.createdAt = createdAt;
+   }
 }
