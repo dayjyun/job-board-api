7a070921f16275343b93a276f149fe7980b76b4f
dayjyun <kbarriosdev@gmail.com>
2023-05-08 20:03:27 -0500

    Generated fields for User class in models package

--- src/main/java/jobboardapi/models/User.java
+++ src/main/java/jobboardapi/models/User.java
@@ -1,4 +1,13 @@
 package jobboardapi.models;
 
 public class User {
+   private Long id;
+
+   
+   private String name;
+
+
+   private String email;
+   private String password;
+   private String resume;
 }
