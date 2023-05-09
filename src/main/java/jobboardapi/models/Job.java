e76f0576717ff590a3d966d41a12d02f6f55dac4
dayjyun <kbarriosdev@gmail.com>
2023-05-08 20:00:23 -0500

    Generated getters and setters for Job class in models package

--- src/main/java/jobboardapi/models/Job.java
+++ src/main/java/jobboardapi/models/Job.java
@@ -30,4 +30,60 @@ public class Job {
       this.applied = applied;
       this.createdAt = createdAt;
    }
+
+   public Long getId() {
+      return id;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public String getTitle() {
+      return title;
+   }
+
+   public void setTitle(String title) {
+      this.title = title;
+   }
+
+   public String getDescription() {
+      return description;
+   }
+
+   public void setDescription(String description) {
+      this.description = description;
+   }
+
+   public String getLocation() {
+      return location;
+   }
+
+   public void setLocation(String location) {
+      this.location = location;
+   }
+
+   public double getSalary() {
+      return salary;
+   }
+
+   public void setSalary(double salary) {
+      this.salary = salary;
+   }
+
+   public boolean isApplied() {
+      return applied;
+   }
+
+   public void setApplied(boolean applied) {
+      this.applied = applied;
+   }
+
+   public Timestamp getCreatedAt() {
+      return createdAt;
+   }
+
+   public void setCreatedAt(Timestamp createdAt) {
+      this.createdAt = createdAt;
+   }
 }
