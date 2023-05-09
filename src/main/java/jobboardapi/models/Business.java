be8c139bde4a1c77402a142a157eb38dba6eb1d3
kimnguyen <knnguyen2410@gmail.com>
2023-05-09 15:28:36 -0500

    Defined many-to-one relationship between business and user models in Business class

--- src/main/java/jobboardapi/models/Business.java
+++ src/main/java/jobboardapi/models/Business.java
@@ -1,5 +1,7 @@
 package jobboardapi.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 
 @Entity
@@ -15,6 +17,12 @@ public class Business {
 
    @Column
    private String headquarters;
+
+   // many businesses can belong to one user
+   @ManyToOne
+   @JoinColumn(name = "user_id")
+   @JsonIgnore // excludes user details when displaying business details
+   private User user;
 
    public Business() {}
 
