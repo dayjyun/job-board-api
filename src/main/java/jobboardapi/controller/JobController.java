3dc92ff14012ac527a1cd734a1aed2628c2f3daa
dayjyun <kbarriosdev@gmail.com>
2023-05-10 20:36:00 -0500

    Created getAllJobs method in JobService and JobController

--- src/main/java/jobboardapi/controller/JobController.java
+++ src/main/java/jobboardapi/controller/JobController.java
@@ -1,9 +1,13 @@
 package jobboardapi.controller;
 
+import jobboardapi.models.Job;
 import jobboardapi.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/jobs")
@@ -11,4 +15,9 @@ public class JobController {
 
     @Autowired
     private JobService jobService;
+
+    @GetMapping(path = "")
+    public List<Job> getAllJobs() {
+        return jobService.getAllJobs();
+    }
 }
--- src/main/java/jobboardapi/service/JobService.java
+++ src/main/java/jobboardapi/service/JobService.java
@@ -1,8 +1,11 @@
 package jobboardapi.service;
 
+import jobboardapi.models.Job;
 import jobboardapi.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class JobService {
@@ -12,5 +15,9 @@ public class JobService {
     @Autowired
     public void setJobRepository(JobRepository jobRepository) {
         this.jobRepository = jobRepository;
+    }
+
+    public List<Job> getAllJobs() {
+        return jobRepository.findAll();
     }
 }
