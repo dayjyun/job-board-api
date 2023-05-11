39f7f9691fb7eb170bcb745df064b595e1b453b7
kimnguyen <knnguyen2410@gmail.com>
2023-05-11 14:59:39 -0500

    Create SecurityConfiguration in security package

---
+++ src/main/java/jobboardapi/security/SecurityConfiguration.java
@@ -0,0 +1,17 @@
+package jobboardapi.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true) // this security configuration applies to entire app configuration
+public class SecurityConfiguration {
+
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    public void setMyUserDetailsService(MyUserDetailsService myUserDetailsService) {
+        this.myUserDetailsService = myUserDetailsService;
+    }
+}
