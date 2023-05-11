e01c8801f200706253a60c181e06125179fdb071
dayjyun <kbarriosdev@gmail.com>
2023-05-11 18:59:51 -0500

    Completed UserService

--- src/main/java/jobboardapi/service/UserService.java
+++ src/main/java/jobboardapi/service/UserService.java
@@ -1,10 +1,16 @@
 package jobboardapi.service;
 
 
+import jobboardapi.exceptions.AlreadyExistsException;
 import jobboardapi.exceptions.NotFoundException;
 import jobboardapi.models.User;
 import jobboardapi.repository.UserRepository;
+import jobboardapi.security.JWTUtils;
+import jobboardapi.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,11 +19,34 @@ import java.util.Optional;
 public class UserService {
 
     private UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private JWTUtils jwtUtils;
+    private AuthenticationManager authenticationManager;
+    private MyUserDetails myUserDetails;
 
     @Autowired
-    public void setUserRepository(UserRepository userRepository) {
+    public UserService(UserRepository userRepository,
+                       @Lazy PasswordEncoder passwordEncoder,
+                       JWTUtils jwtUtils,
+                       @Lazy AuthenticationManager authenticationManager,
+                       @Lazy MyUserDetails myUserDetails) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+        this.authenticationManager = authenticationManager;
+        this.myUserDetails = myUserDetails;
     }
+
+    public User createUser(User userObject) {
+        if (!userRepository.existsByEmail(userObject.getEmail())) {
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            return userRepository.save(userObject);
+        } else {
+            throw new AlreadyExistsException("user with email address " + userObject.getEmail() +
+                    " already exists");
+        }
+    }
+
 
     public User findUserByEmailAddress(String email) {
         return userRepository.findUserByEmail(email);
