0b97f694794738e202445e7b48a2c6147add7c1f
kimnguyen <knnguyen2410@gmail.com>
2023-05-11 15:03:54 -0500

    Create JWTRequestFilter in security package

---
+++ src/main/java/jobboardapi/security/JWTUtils.java
@@ -0,0 +1,10 @@
+package jobboardapi.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTUtils {
+
+
+
+}
--- src/main/java/jobboardapi/security/SecurityConfiguration.java
+++ src/main/java/jobboardapi/security/SecurityConfiguration.java
@@ -1,8 +1,21 @@
 package jobboardapi.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.context.WebApplicationContext;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true) // this security configuration applies to entire app configuration
@@ -14,4 +27,5 @@ public class SecurityConfiguration {
     public void setMyUserDetailsService(MyUserDetailsService myUserDetailsService) {
         this.myUserDetailsService = myUserDetailsService;
     }
+
 }
