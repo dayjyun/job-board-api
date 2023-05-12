4f14e76692a8bfcb0b3e7bf73cfec7d7ec47134b
dayjyun <kbarriosdev@gmail.com>
2023-05-11 20:49:21 -0500

    Added @Valid annotation in createUser in UserController to prevent a new account from being created if user did not provide sufficient data to create account

--- src/main/java/jobboardapi/controller/UserController.java
+++ src/main/java/jobboardapi/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
@@ -25,7 +26,7 @@ public class UserController {
 
     // http://localhost:8080/auth/users/register
     @PostMapping("/register")
-    public User createUser(@RequestBody User userObject) {
+    public User createUser(@RequestBody @Valid User userObject) {
         return userService.createUser(userObject);
     }
 
