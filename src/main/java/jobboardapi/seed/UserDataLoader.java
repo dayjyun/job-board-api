1f3ccd15a4a5b5777403adf2d70e4009db041e5e
kimnguyen <knnguyen2410@gmail.com>
2023-05-12 11:50:02 -0500

    Used createUser method from user service to create user seed data

--- src/main/java/jobboardapi/seed/UserDataLoader.java
+++ src/main/java/jobboardapi/seed/UserDataLoader.java
@@ -2,12 +2,16 @@ package jobboardapi.seed;
 
 import jobboardapi.models.User;
 import jobboardapi.repository.UserRepository;
+import jobboardapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserDataLoader implements CommandLineRunner {
+
+    @Autowired
+    UserService userService;
 
     @Autowired
     UserRepository userRepository;
@@ -22,9 +26,9 @@ public class UserDataLoader implements CommandLineRunner {
             User user1 = new User(1L, "DeShe", "deshe@gmail.com", "pw", "resume1");
             User user2 = new User(2L, "Kevin", "kevin@gmail.com", "pw", "resume2");
             User user3 = new User(3L, "Kim", "kim@gmail.com", "pw", "resume3");
-            userRepository.save(user1);
-            userRepository.save(user2);
-            userRepository.save(user3);
+            userService.createUser(user1);
+            userService.createUser(user2);
+            userService.createUser(user3);
         }
     }
 }
