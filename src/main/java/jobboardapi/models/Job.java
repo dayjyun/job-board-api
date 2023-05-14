63dccc4cc57b5d4c7ca408f18908b14a376f35aa
dayjyun <kbarriosdev@gmail.com>
2023-05-13 23:05:19 -0500

    Removed unused code in Job model

--- src/main/java/jobboardapi/models/Job.java
+++ src/main/java/jobboardapi/models/Job.java
@@ -1,11 +1,7 @@
 package jobboardapi.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.hibernate.annotations.LazyCollection;
-import org.hibernate.annotations.LazyCollectionOption;
-
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.List;
 import javax.validation.constraints.NotNull;
 
@@ -31,27 +27,11 @@ public class Job {
    @Column
    private double salary;
 
-//   @Column
-//   private boolean applied;
-
-//   // many job applications can belong to one user
-//   // Current logged-in user can see the list of jobs they applied for
-//   @ManyToOne
-//   @JoinColumn(name = "user_id")
-//   @JsonIgnore // excludes user details when displaying job details
-//   private User user;
-
    // many job listings can belong to one business
    @ManyToOne
    @JoinColumn(name = "business_id")
    @JsonIgnore // excludes business details when displaying job details
    private Business business;
-
-//   // Job should have a list of users who applied to the job listing
-//   // one job can have many applicants (users)
-//   @OneToMany(mappedBy = "job", orphanRemoval = true)
-//   @LazyCollection(LazyCollectionOption.FALSE)
-//   private List<User> applicantsList;
 
    @ManyToMany
    @JoinTable(name = "user_jobs",
@@ -69,8 +49,6 @@ public class Job {
       this.description = description;
       this.location = location;
       this.salary = salary;
-//      this.applied = applied;
-//      this.applicantsList = new ArrayList<>();
    }
 
    public Long getId() {
@@ -113,23 +91,6 @@ public class Job {
       this.salary = salary;
    }
 
-//   public boolean isApplied() {
-//      return applied;
-//   }
-//
-//   public void setApplied(boolean applied) {
-//      this.applied = applied;
-//   }
-
-
-//   public User getUser() {
-//      return user;
-//   }
-//
-//   public void setUser(User user) {
-//      this.user = user;
-//   }
-
    public Business getBusiness() {
       return business;
    }
@@ -154,7 +115,6 @@ public class Job {
               ", description='" + description + '\'' +
               ", location='" + location + '\'' +
               ", salary=" + salary +
-//              ", applied=" + applied +
               '}';
    }
 }
