5483caeca4abad04500aed88aaa0cd086b59a85b
dayjyun <kbarriosdev@gmail.com>
2023-05-14 02:54:40 -0500

    Created comments in MyProfileController

--- src/main/java/jobboardapi/controller/MyProfileController.java
+++ src/main/java/jobboardapi/controller/MyProfileController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/myProfile")
@@ -16,21 +15,29 @@ public class MyProfileController {
    @Autowired
    private MyProfileService myProfileService;
 
+   // Functionality: Returns logged-in user's details
+   // Path: http://localhost:8080//api/myProfile
    @GetMapping(path = "")
    public User getMyProfile() {
       return MyProfileService.getLoggedInUser();
    }
 
+   // Functionality: Edit user account
+   // Path: http://localhost:8080//api/myProfile
    @PutMapping(path = "")
    public User updateMyProfile(@RequestBody User updatedBody) {
       return myProfileService.updateMyProfile(updatedBody);
    }
 
+   // Functionality: Delete user account
+   // Path: http://localhost:8080//api/myProfile
    @DeleteMapping(path = "")
    public JSONObject deleteMyProfile() {
       return myProfileService.deleteMyProfile();
    }
 
+   // Functionality: Returns a list of jobs the user applied for
+   // Path: http://localhost:8080//api/myProfile/jobs
    @GetMapping(path = "/jobs")
    public List<Job> getJobsIAppliedFor() {
       return myProfileService.getJobsIAppliedFor();
