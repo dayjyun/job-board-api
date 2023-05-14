ec2d8a60747e159a66d0e8787e5225f49868993d
dayjyun <kbarriosdev@gmail.com>
2023-05-13 21:46:14 -0500

    Updated JobRepository

--- src/main/java/jobboardapi/repository/JobRepository.java
+++ src/main/java/jobboardapi/repository/JobRepository.java
@@ -10,6 +10,6 @@ import java.util.Optional;
 public interface JobRepository extends JpaRepository<Job, Long> {
 //   List<User> findAllByApplicantsList(List<User> applicantsList)findAllByApplicantsList(List<User> applicantsList);
 //   Optional<User> findByUserEmail(String email);
-//   Optional<User> findByUserId(Long userId);
-//   Optional<User> findJobByIdAndAndUserId(Long jobId, Long userId);
+   Optional<User> findByUserId(Long userId);
+   Optional<User> findJobByIdAndAndUserId(Long jobId, Long userId);
 }
