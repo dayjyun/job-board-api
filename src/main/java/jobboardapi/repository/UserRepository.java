f7505b7186ffdcbbee33901a9b0ab16338ad6829
dayjyun <kbarriosdev@gmail.com>
2023-05-15 01:09:10 -0500

    Removed unused code in UserRepository

--- src/main/java/jobboardapi/repository/UserRepository.java
+++ src/main/java/jobboardapi/repository/UserRepository.java
@@ -3,8 +3,6 @@ package jobboardapi.repository;
 import jobboardapi.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
    // register
    boolean existsByEmail(String email);
